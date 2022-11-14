787d106e817695d2899ba8329e3b93f96e89afb4
liuchenxi <liuchenxi@yuanian.com>
2022-11-14 16:03:08 +0800

    11.14

--- 我的代码/3.解构赋值/3.js
+++ 我的代码/3.解构赋值/3.js
@@ -5,7 +5,18 @@
 // let { p, p: [x, { y }] } = {p: ["Hello", { y: "World" }]};
 // console.log(x,y,p);
 
-const node = {loc: {start: {line: 1,column: 5}}};
+// const node = {loc: {start: {line: 1,column: 5}}};
 
-  let {loc,loc:{start},loc:{start:{line,column}}} = node
-  console.log(loc,start,line,column);
+//   let {loc,loc:{start},loc:{start:{line,column}}} = node
+//   console.log(loc,start,line,column);
+
+
+function move({x = 0, y = 0} = {}) {
+    return [x, y];
+  }
+  
+  console.log(move({x: 3, y: 8}),move({x: 3}),move({}),move());
+  move({x: 3, y: 8}); // [3, 8]
+  move({x: 3}); // [3, 0]
+  move({}); // [0, 0]
+  move(); // [0, 0]
